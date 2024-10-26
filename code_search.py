280836ef811168edd10e6da64fd1c373a98dc226
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-10-26 09:02:11 +0000

    add search repo

--- code_search.py
+++ code_search.py
@@ -20,6 +20,23 @@ def search_github_code(keyword, token):
         print(f"An error occurred: {e}")
         return None
 
+def search_github_repositories(keyword, token):
+    url = f"https://api.github.com/search/repositories?q={keyword}&sort=stars&order=desc&per_page=100"
+
+    headers = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
 if __name__ == "__main__":
     if len(sys.argv) < 1:
         print("Usage: python github_search.py <keyword> ")
