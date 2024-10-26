0a839a89f09f1348b238648f59724fa8df0a73cf
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2024-10-26 09:27:16 +0000

    streamlined

--- code_search.py
+++ code_search.py
@@ -3,25 +3,8 @@ import sys
 import os
 import json
 
-def search_github_code(keyword, token):
-    url = f"https://api.github.com/search/code?q={keyword}&sort=indexed&order=desc&per_page=100"
-
-    headers = {
-        "Authorization": f"token {token}",
-        "Accept": "application/vnd.github.v3+json"
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        return data
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None
-
-def search_github_repositories(keyword, token):
-    url = f"https://api.github.com/search/repositories?q={keyword}&sort=stars&order=desc&per_page=100"
+def search_github(action, keyword, token):
+    url = f"https://api.github.com/search/{action}?q={keyword}&sort=indexed&order=desc&per_page=100"
 
     headers = {
         "Authorization": f"token {token}",
@@ -38,16 +21,27 @@ def search_github_repositories(keyword, token):
         return None
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
-        print("Usage: python github_search.py <keyword> ")
+    if len(sys.argv) < 3:
+        print("Usage: python github_search.py <code | repositories | issues | topics> <keyword>")
         sys.exit(1)
 
-    keyword = sys.argv[1]
+    action = sys.argv[1]
+    keyword = sys.argv[2]
     token = os.getenv("GITHUB_TOKEN")
+
     if not token:
         print("Error: GITHUB_TOKEN environment variable not set.")
         sys.exit(1)
 
-    result = search_github_code(keyword, token)
+    if action not in ["code", "repositories", "issues", "topics"]:
+        print("Error: Action must be either 'code' or 'repositories'.")
+        sys.exit(1)
+
+    if action:
+        result = search_github(action, keyword, token)
+
+
     if result:
         print(json.dumps(result, indent=4))
+    else:
+        print("No results found or an error occurred.")
